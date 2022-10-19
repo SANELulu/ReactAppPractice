5ac5ab74056cdb6ba6fc1ffabd7d3872478488cb
Lucas Diaz <lucasdiaz22b@gmail.com>
2022-10-18 21:54:55 -0400

    New Expense component added

--- practice-app/src/App.js
+++ practice-app/src/App.js
@@ -1,3 +1,5 @@
+import React from "react";
+import NewExpense from "./components/NewExpense/NewExpense";
 import Expenses from "./components/Expenses/Expenses";
 
 const App = () => {
@@ -24,6 +26,7 @@ const App = () => {
   ];
   return (
     <div>
+      <NewExpense />
       <Expenses items={expenses} />
     </div>
   );
