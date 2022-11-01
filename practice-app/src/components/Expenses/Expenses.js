b1f7444869961488659de5a1495d6ad0c9ca7c6c
Lucas Diaz <lucasdiaz22b@gmail.com>
2022-10-31 21:00:31 -0400

    chart dataPoint functionality

--- practice-app/src/components/Expenses/Expenses.js
+++ practice-app/src/components/Expenses/Expenses.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import ExpensesFilter from "./ExpensesFilter";
 import ExpensesList from "./ExpensesList";
 import Card from "../UI/Card";
+import ExpensesChart from "./ExpensesChart";
 import "./Expenses.css";
 
 const Expenses = (props) => {
@@ -21,6 +22,7 @@ const Expenses = (props) => {
         selected={chosenYear}
         onFilteredYear={filterChangeHandler}
       />
+      <ExpensesChart expenses={filteredExpenses} />
       <ExpensesList items={filteredExpenses} />
     </Card>
   );
